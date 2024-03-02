d0dc6c2753d64d9c8aa6288808f46170fb42cda5
unknown <notxdead0@gmail.com>
2024-03-02 13:11:27 +0530

    before.final
    content are missing

--- 2024/2024.js
+++ 2024/2024.js
@@ -23,15 +23,3 @@ $(document).ready(function(){
 	);
 	});
 
-	window.onscroll = function() {myFunction()};
-
-	var navbar = document.getElementById("navbar");
-	var sticky = navbar.offsetTop;
-  
-	function myFunction() {
-	  if (window.pageYOffset >= sticky) {
-		navbar.classList.add("sticky");
-	  } else {
-		navbar.classList.remove("sticky");
-	  }
-	}
